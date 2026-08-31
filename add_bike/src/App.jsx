cbb9467348a4f251b654b5277737cc7d937b9359
IDRJekyll <markogrmusa04@gmail.com>
2026-09-01 01:01:06 +0200

    DB Bike Display

--- add_bike/src/App.jsx
+++ add_bike/src/App.jsx
@@ -1,139 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import MotoCard from "./components/motocard";
+import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
-  const [manufacturer, setManufacturer] = useState("");
-  const [engine, setEngine] = useState("");
-  const [power, setPower] = useState("");
-  const [fuel, setFuel] = useState("");
-  const [weight, setWeight] = useState("");
-  const [image, setImage] = useState("");
+  const [bikes, setBikes] = useState([]);
 
-  const [message, setMessage] = useState("");
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const motorcycle = {
-      name: name,
-      manufacturer: manufacturer,
-      engine_cc: engine,
-      power_kw: power,
-      fuel: fuel,
-      weight: weight,
-      image: image
-    };
-
-    console.log("Sending:", motorcycle);
-
-    const response = await fetch(
-      "http://localhost/motorwebsite/api/add_bike.php",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(motorcycle)
-      }
-    );
-
-    const data = await response.json();
-
-    console.log("PHP responded:", data);
-
-    if (data.success) {
-      setMessage("Your motorcycle has been added successfully.")
-
-      setName("");
-      setManufacturer("");
-      setEngine("");
-      setPower("");
-      setFuel("");
-      setWeight("");
-      setImage("");
-    }
-    else {
-      setMessage("Failed to add due to error. Please try again later.")
-    }
-  }
+  useEffect(() => {
+    fetch("http://localhost/motorwebsite/api/get_bikes.php")
+      .then(response => response.json())
+      .then(data => {
+        console.log("Bikes received:", data);
+        setBikes(data);
+      });
+  }, []);
 
   return (
-    <div>
-      <h1>Add a Motorcycle</h1>
+    <div className="specPage">
+      <h1>Motorcycles</h1>
 
-      <form onSubmit={handleSubmit}>
-
-        <div>
-          <label>Name</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
+      {
+        bikes.map((bike) => (
+          <MotoCard
+            key={bike.bikeID}
+            bike={bike}
           />
-        </div>
-
-        <div>
-          <label>Manufacturer</label>
-          <input
-            type="text"
-            value={manufacturer}
-            onChange={(event) => setManufacturer(event.target.value)}
-          />
-        </div>
-
-        <div>
-          <label>Engine (cc)</label>
-          <input
-            type="number"
-            value={engine}
-            onChange={(event) => setEngine(event.target.value)}
-          />
-        </div>
-
-        <div>
-          <label>Power (kW)</label>
-          <input
-            type="number"
-            step="0.01"
-            value={power}
-            onChange={(event) => setPower(event.target.value)}
-          />
-        </div>
-
-        <div>
-          <label>Fuel</label>
-          <input
-            type="text"
-            value={fuel}
-            onChange={(event) => setFuel(event.target.value)}
-          />
-        </div>
-
-        <div>
-          <label>Weight (kg)</label>
-          <input
-            type="number"
-            value={weight}
-            onChange={(event) => setWeight(event.target.value)}
-          />
-        </div>
-
-        <div>
-          <label>Image</label>
-          <input
-            type="text"
-            value={image}
-            onChange={(event) => setImage(event.target.value)}
-          />
-        </div>
-
-        <button type="submit">
-          Add Motorcycle
-        </button>
-
-        {message && <p>{message}</p>}
-      </form>
-
+        ))
+      }
     </div>
   );
 }
--- api/get_bikes.php
+++ api/get_bikes.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:5174");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Content-Type: application/json");
 
 require_once "../config/db_connect.php";
