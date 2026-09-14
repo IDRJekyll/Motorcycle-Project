1fa6a6599f4d0bb7a64d372cf78b1984867eda64
IDRJekyll <markogrmusa04@gmail.com>
2026-09-14 11:31:34 +0200

    Added Navbar

--- add_bike/src/App.jsx
+++ add_bike/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import MotoCard from "./components/motocard";
+import Navbar from "./components/navbar";
 import "./App.css";
+
 
 function App() {
   const [bikes, setBikes] = useState([]);
@@ -21,25 +23,28 @@ function App() {
   );
 
   return (
-    <div className="specPage">
-      <h1>Motorcycles</h1>
+    <>
+      <Navbar />
+      <div className="specPage">
+        <h1>Motorcycles</h1>
 
-      <div className="searchBar">
-        <input
-          type="text" placeholder="Search Bikes"
-          value={search}
-          onChange={(event) => setSearch(event.target.value)}
-        />
-      </div>
-      {
-        filterBikes.map((bike) => (
-          <MotoCard
-            key={bike.bikeID}
-            bike={bike}
+        <div className="searchBar">
+          <input
+            type="text" placeholder="Search Bikes"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
           />
-        ))
-      }
-    </div>
+        </div>
+        {
+          filterBikes.map((bike) => (
+            <MotoCard
+              key={bike.bikeID}
+              bike={bike}
+            />
+          ))
+        }
+      </div>
+    </>
   );
 }
 
---
+++ add_bike/src/components/navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <a href="/">Home</a>
+            <a href="/bikes">Bikes</a>
+            <a href="/gear">Gear</a>
+        </nav>
+    );
+}
+export default Navbar;
